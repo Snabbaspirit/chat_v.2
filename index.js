3223059e79896cdfc808b1bfe72b24a19f73fa26
Ivan Kuzmin <snobbaspirit@gmail.com>
2021-06-29 21:29:20 +0300

    videoplayer component ready

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const io = require('socket.io')(server, {
 
 app.use(cors());
 
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (request, response) => {
   response.send('Server started')
